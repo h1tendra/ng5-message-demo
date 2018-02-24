291a450ab8209bc6ddf034d1e1b04a61f589ec86
h1tendra <hitendra.http@gmail.com>
2018-02-24 16:00:36 +0530

    increase server polling time

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 export const config: any = {
   API_ENDPOINT: 'http://localhost:4200/assets/api/',
-  POLLING_INTERVAL: 1000 * 2, // 2 seconds (Milliseconds)
+  POLLING_INTERVAL: 1000 * 5, // In Milliseconds
   OVERRIDE_SERVER_EXPIRE_TIME: true,
 };
